6f06a2a7cf5b0caead50281b2f38ba77ac2859b3
Gao Xiang <2018020488@qdu.edu.cn>
2019-12-16 12:15:27 +0800

    add some codes

---
+++ findObject.js
@@ -0,0 +1,7 @@
+//1.test click
+// var widget=text('赞3333').findOne( 5000);
+// click(widget.bounds().centerX(), widget.bounds().centerY());
+var text =id('com.tencent.mm:id/ezr').findOne(2000);
+text.setText('hello china');
+log(text);
+
