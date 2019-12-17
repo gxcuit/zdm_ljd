bf91de5e4abf923e7e95320a4573d0abad1cdd58
Gao Xiang <2018020488@qdu.edu.cn>
2019-12-17 13:59:55 +0800

    微信自动点赞

--- findObject.js
+++ findObject.js
@@ -1,7 +1,32 @@
-//1.test click
-// var widget=text('赞3333').findOne( 5000);
-// click(widget.bounds().centerX(), widget.bounds().centerY());
-var text =id('com.tencent.mm:id/ezr').findOne(2000);
-text.setText('hello china');
-log(text);
+/**
+ * 微信自动点赞
+ */
+
+
+function like() {
+    var lv = className('ListView').findOnce();
+    var lv_child = lv.children();
+    lv_child.forEach(element => {
+        //log(element);
+        var comment = element.findOne(desc('评论'));
+        if (comment) {
+            comment.click();
+            sleep(1000);
+            var zan  = text('赞').findOnce();
+            if(zan){
+                zan.parent().click();
+            }
+            sleep(1000);
+        }
+    });
+    //v.scrollDown();
+    scrollDown();
+    sleep(1000);
+}
+
+for (let index = 0; index < 10; index++) {
+    like();
+}
+
+
 
