ea723490c98fc9b68aa5117c0c6139baa6fe5a8f
Gao Xiang <2018020488@qdu.edu.cn>
2019-12-31 10:31:47 +0800

    改sleep为随机

--- zdm_jd _5times_aday.js
+++ zdm_jd _5times_aday.js
@@ -1,6 +1,6 @@
 //TODO: 注释
 function follow() {
-    sleep(1000);
+    sleep(random(1000, 3000));
     var follow = id('com.jd.lib.jshop:id/ue').findOne(3000);
     if (!follow) {
         toastLog("不存在'关注'或'关注有礼！'");
@@ -8,7 +8,7 @@ function follow() {
         return;
     }
     follow.click();
-    sleep(1000);
+    sleep(random(2000, 2500));
 
     //var confirm = text('确认').findOne(1000);
     var confirm = textMatches("/(确认)+|(收下)+|(关注)+/").findOne(2000);
@@ -20,28 +20,16 @@ function follow() {
     } else {
         toastLog('暂无京豆---领取失败！');
     }
-    sleep(1000);
+    sleep(random(1000, 2000));
     back();
 }
-
-
-
-
 auto.waitFor();
 toastLog('start');
-//var receive =textContains('点此领取').clickable().find();
-
-
-
-
-
-//var receive =textMatches("点此领取\\d+").clickable().find();
-//toastLog('当前界面共找到点击领取* '+receive.size()+' 个');
 var gv_child = className('GridView').findOne(2000).children();
 console.log(gv_child.size());
 
 gv_child.forEach(view => {
-    sleep(random(900,1500));
+    sleep(random(900, 1500));
     var getjd = view.findOne(clickable());
     if (!getjd)
         return;
@@ -52,12 +40,9 @@ gv_child.forEach(view => {
     var jd = id('android:id/text1').text('京东').findOne(1000);
 
 
-        jd.parent().click();
-        follow();
-
-
+    jd.parent().click();
+    follow();
 });
-
 if (gv_child.empty()) {
     console.log('empty');
     scrollDown();
